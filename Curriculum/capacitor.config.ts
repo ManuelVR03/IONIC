8538122b54aad1aae09ab4da8750a317bef90423
Manuel <manuel.verdejo.ramirez.alu@iesjulioverne.es>
2023-09-26 11:01:41 +0200

    Renovado

---
+++ Curriculum/capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Curriculum',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
